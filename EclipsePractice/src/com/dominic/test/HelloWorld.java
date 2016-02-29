4dd69e7c39e82aeea2c0f2433c792295c3115b0d
ddublin <USER@Dominic.fastallaght.com>
2016-02-29 11:53:54 +0000

    second commit

--- EclipsePractice/src/com/dominic/test/HelloWorld.java
+++ EclipsePractice/src/com/dominic/test/HelloWorld.java
@@ -1,9 +1,14 @@
 package com.dominic.test;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
+		System.out.println("Hello please type something and press return");		
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		System.out.println("You said "+input);
+		System.out.println("The lenght of your input is "+input.length());
 	}
-
 }
