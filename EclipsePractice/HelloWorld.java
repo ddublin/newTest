6142c92c3fd2fd3f8a4b65c1ff501a6ad933c2d9
ddublin <USER@Dominic.fastallaght.com>
2016-02-29 10:51:46 +0000

    second commit

--- EclipsePractice/HelloWorld.java
+++ EclipsePractice/HelloWorld.java
@@ -3,7 +3,6 @@ package com.dominic.test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Github");
-		
+		System.out.println("Hello Github");		
 	}
 }
